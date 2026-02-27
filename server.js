ace21ba54afb35c895eee0d22f4703e9fe2b9ee8
Sumit Shah <sumit@example.com>
2026-02-28 04:09:00 +0530

    Fixed MySQL connection

--- server.js
+++ server.js
@@ -9,19 +9,14 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 // MySQL connection
-const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
-});
+
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect((err) => {
   if (err) {
     console.error("Database connection failed:", err);
   } else {
-    console.log("MySQL Connected...");
+    console.log("Connected to Railway MySQL");
   }
 });
 
